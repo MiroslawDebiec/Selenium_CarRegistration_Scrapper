eb41f7acb7931bad529a8d163f80c94f0fedc0b4
Miroslaw Debiec <debiec.miroslaw@gmail.com>
2018-10-16 19:41:57 +0100

    First upload

---
+++ Registration Scrapper -Selenium/Selenium/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium
+{
+    class Car
+    {
+        public string Make { get; set; }
+        public string Color { get; set; }
+        public string Year { get; set; }
+        public string Registration { get; set; }
+        public string Mot { get; set; }
+        public string Tax { get; set; }
+
+        public Car()
+        {
+            this.Make = "Unknown";
+            this.Color = "Unknown";
+            this.Registration = "Unknown"; 
+            this.Year = "Unknown";
+            this.Mot = "Unknown";
+            this.Tax = "Unknown"; 
+        }
+
+        public Car(string _registration)
+        {
+            this.Registration = _registration;
+        }
+    }
+}
---
+++ Registration Scrapper -Selenium/Selenium/Program.cs
@@ -0,0 +1,58 @@
+//using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var options = new ChromeOptions();
+            //options.AddArgument("--disable-gpu");
+            //var driver = new ChromeDriver(options);
+            var driver = new PhantomJSDriver();
+            var car = new Car();
+            bool isEnd = false;
+            string carRegistration = "";
+
+            while (isEnd == false)
+            {
+                try
+                {
+                    Console.WriteLine("------------------------------------------------");
+                    Console.Write("ENETR VALID UK CAR REGISTRATION: ");
+                    car.Registration = Console.ReadLine();
+                    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+                    driver.Url = "https://vehicleenquiry.service.gov.uk/";
+
+                    driver.FindElementById("Vrm").SendKeys(car.Registration);
+                    driver.FindElementByClassName("button").Click();
+                    driver.FindElementByXPath("//*[@id=\"Correct_True\"]").Click();
+                    driver.FindElementByClassName("button").Click();
+                    car.Make = (driver.FindElementByXPath("//*[@id=\"content\"]/div[4]/div/ul/li[1]/span[2]/strong")).Text;
+                    car.Color = (driver.FindElementByXPath("//*[@id=\"content\"]/div[4]/div/ul/li[10]/span[2]/strong")).Text;
+                    car.Year = (driver.FindElementByXPath("//*[@id=\"content\"]/div[4]/div/ul/li[3]/span[2]/strong")).Text;
+                    car.Tax = (driver.FindElementByXPath("//*[@id=\"content\"]/div[2]/div[1]/div[1]/p/strong")).Text; 
+                    car.Mot = (driver.FindElementByXPath("//*[@id=\"content\"]/div[2]/div[2]/div[1]/p/strong")).Text;
+
+                    Console.WriteLine("------------------------------------------------");
+                    Console.WriteLine(" * REGISTRATION: {0}\n * MAKE: {1}\n * COLOR: {2}\n * YEAR: {3}\n * TAX: {4}\n * MOT: {5}", car.Registration, car.Make, car.Color, car.Year, car.Tax, car.Mot);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("------------------------------------------------");
+                    Console.WriteLine("      !!! INVALID REGISTRATION ENTERED !!!");
+                }
+            }
+            driver.Close();
+        }
+    }
+}
